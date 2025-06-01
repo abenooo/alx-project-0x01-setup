fd7e1858773dd6412af5bfeef44d2203efdca6f7
Abenezer Kifle <abenezerkifle000@gmail.com>
2025-06-01 11:13:41 -0700

    Update Users component to fetch and return posts instead of users, adjusting static props accordingly.

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -41,16 +41,14 @@ const Users: React.FC<{ users: UserProps[] }> = ({ users }) => {
     </div>
   );
 };
-
-export default Users;
-
 export async function getStaticProps() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users = await response.json();
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const posts = await response.json()
 
   return {
     props: {
-      users,
-    },
-  };
+      posts
+    }
+  }
 }
+export default Users;
